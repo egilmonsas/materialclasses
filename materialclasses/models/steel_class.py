c16a2eeea0d8d8e032f50d5c1e8effa5bfa5820d
Vasse <egilmonsas@gmail.com>
2022-11-08 20:14:11 +0100

    added some fields to steel class

--- materialclasses/models/steel_class.py
+++ materialclasses/models/steel_class.py
@@ -2,8 +2,17 @@ from pydantic import BaseModel, Field, validator
 
 
 class SteelClass(BaseModel):
+    # Metainfo
     standard: str = Field(description="Governing design standard")
     variant: str = Field(description="Name of the steel class")
+
+    # Material constants
+    density: int = Field(description="Bulk density", default=7800, ge=0)
+    E: int = Field(description="Youngs Modulus", default=210000)
+    poisson: int = Field(description="Possion ratio", default=0.3, ge=0, le=1)
+    G: int = Field(description="G", default=81000)
+
+    # Strength parameters
     fy: int = Field(description="Yield strength, t<=40", ge=0)
     fu: int = Field(description="Ultimate strength, t<=40", ge=0)
     fy_t: int | None = Field(description="Yield strength, 40<t<=80", ge=0)
